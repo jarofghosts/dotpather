d58e51241cdfb52255b67c04273f5e2327001cc1
jesse keane <me@jessekeane.me>
2014-03-01 12:06:12 -0800

    minor improvements

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-module.exports = dot_path
+module.exports = dotpath
 
-function dot_path(str) {
-  var parts = str.toString().split('.'),
-      l = parts.length
+function dotpath(str) {
+  var parts = str.toString().split('.')
+    , len = parts.length
   
   return function parse(obj) {
     var test_key
 
-    if (obj == undefined) return undefined
+    if (obj === undefined) return undefined
     
-    for (var i = 0; i < l; ++i) {
+    for (var i = 0; i < len; ++i) {
       test_key = parts[i]
-      if (obj[test_key] === undefined) return undefined
+
+      if (!obj || obj[test_key] === undefined) return undefined
       obj = obj[test_key]
     }
 
