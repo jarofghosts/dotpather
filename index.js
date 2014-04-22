15c47b70f76c471d2657def0ed7f7893c6f0bc22
jesse keane <me@jessekeane.me>
2014-04-21 21:10:29 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 module.exports = dotpath
 
 function dotpath(str) {
-  var parts = str.toString().split('.')
+  var parts = ('' + str).split('.')
     , len = parts.length
   
   return function parse(obj) {
     var test_key
     
-    for (var i = 0; i < len; ++i) {
+    for(var i = 0; i < len; ++i) {
       test_key = parts[i]
 
-      if (!obj) return undefined
+      if(!obj) return
+
       obj = obj[test_key]
     }
 
